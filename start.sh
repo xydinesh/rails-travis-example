f16e01bee47c104bf86610aaec75577b1b2a4f1e
James Wickett <wickett@gmail.com>
2014-06-20 23:13:28 -0500

    testing start

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 ./set_token.sh
-bundle exec start_services > /dev/null 2>&1 &
+bundle exec start_services &
 TRIAGE_PID=$!
 echo "Triage app started at $TRIAGE_PID PID and is available at localhost:3000"
